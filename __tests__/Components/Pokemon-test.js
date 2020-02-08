8a2c80d5f3ce4574816684a30c07f3b76d08c5b3
Egie Imandha <egieimandha@gmail.com>
2020-02-08 09:52:15 +0700

    Add: unit testing for component pokemons

---
+++ __tests__/Components/Pokemon-test.js
@@ -0,0 +1,228 @@
+import React from "react"
+import "react-native"
+import {
+  RenderDetailWithOneValuePokemon,
+  RenderTypeName,
+  RenderDetailWithMultipleValuePokemon,
+  RenderDetailPokemon,
+  RenderPokemon,
+  RenderListPokemon,
+  RenderContainerDetailPokemon
+} from '../../screens/ComponentPokemon'
+import BodyRegular from "../../components/UI/BodyRegular"
+import renderer from 'react-test-renderer'
+
+
+describe('Components Pokemon', () => {
+  const mockHandleRefresh = jest.fn()
+  const mockHandleLoadMore = jest.fn()
+  const mockHandleSelectedPokemon = jest.fn()
+  const mockPokemons = [
+    {
+      "id": "4",
+      "imageUri": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/4.png",
+      "name": "charmander",
+    },
+   {
+      "id": "5",
+      "imageUri": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/5.png",
+      "name": "charmeleon",
+    },
+   {
+      "id": "6",
+      "imageUri": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/6.png",
+      "name": "charizard",
+    }
+  ]
+  const mockSelectedPokemonId = 6
+  const mockSelectedPokemon = {
+    name: 'charizard',
+    height: '17',
+    weight: '905',
+    types: [
+      {
+        slot: 1,
+        type: {
+          name: 'Flying'
+        }
+      },
+      {
+        slot: 2,
+        type: {
+          name: 'Fire'
+        }
+      }
+    ]
+  }
+  const mockRenderItemPokemon = ({ item }) => {
+    return <RenderPokemon
+      pokemon={item}
+      handleSelectedPokemon={mockHandleSelectedPokemon}
+      selectedPokemonId={mockSelectedPokemonId}
+    />
+  }
+  describe('Render Detail With One Value Pokemon snapshot', () => {
+    const mockText = 'Height'
+    const mockHeight = '17'
+    const testRenderDetailWithOneValuePokemon = renderer.create(
+      <RenderDetailWithOneValuePokemon text={mockText} value={mockSelectedPokemon.height} />
+    )
+    it('should be match to snapsot', () => {
+      expect(testRenderDetailWithOneValuePokemon.toJSON()).toMatchSnapshot()
+    })
+    it('text must be same with props', () => {
+      const instance = testRenderDetailWithOneValuePokemon.root
+      expect(instance.findAllByType(BodyRegular)[0].props.children).toBe(mockText)
+    })
+    it('value must be same with props', () => {
+      const instance = testRenderDetailWithOneValuePokemon.root
+      expect(instance.findAllByType(BodyRegular)[1].props.children).toBe(mockSelectedPokemon.height)
+    })
+    it('should be render nothing if props is empty', () => {
+      const testRenderDetailWithOneValuePokemon = renderer.create(
+        <RenderDetailWithOneValuePokemon />
+      )
+      expect(testRenderDetailWithOneValuePokemon.toJSON()).toMatchSnapshot()
+    })
+  })
+  describe('Render Type Name', () => {
+    const testRenderTypeName = renderer.create(
+      <RenderTypeName name={mockSelectedPokemon.types[0].type.name} />
+    )
+    it('should be match to snapsot', () => {
+      expect(testRenderTypeName.toJSON()).toMatchSnapshot()
+    })
+    it('name must be same with props', () => {
+      const instance = testRenderTypeName.root
+      expect(
+        instance.findAllByType(BodyRegular)[0].props.children)
+          .toBe(mockSelectedPokemon.types[0].type.name.toUpperCase()
+      )
+    })
+    it('should be render nothing with label X if have prop isFilter', () => {
+      const testRenderTypeName = renderer.create(
+        <RenderTypeName name={mockSelectedPokemon.types[0].type.name} isFilter />
+      )
+      expect(testRenderTypeName.toJSON()).toMatchSnapshot()
+    })
+    it('should be render nothing if props is empty', () => {
+      const testRenderTypeName = renderer.create(
+        <RenderTypeName />
+      )
+      expect(testRenderTypeName.toJSON()).toMatchSnapshot()
+    })
+  })
+  describe('Render Detail With Multiple Value Pokemon snapshot', () => {
+    const mockText = 'Type'
+    const testRenderDetailWithMultipleValuePokemon = renderer.create(
+      <RenderDetailWithMultipleValuePokemon text={mockText} value={mockSelectedPokemon.types} />
+    )
+    it('should be match to snapsot', () => {
+      expect(testRenderDetailWithMultipleValuePokemon.toJSON()).toMatchSnapshot()
+    })
+    it('name must be same with props', () => {
+      const instance = testRenderDetailWithMultipleValuePokemon.root
+      expect(instance.findAllByType(BodyRegular)[0].props.children).toBe(mockText)
+    })
+    it('name type must be same with props', () => {
+      const instance = testRenderDetailWithMultipleValuePokemon.root
+      expect(
+        instance.findAllByType(BodyRegular)[1].props.children)
+          .toBe(mockSelectedPokemon.types[0].type.name.toUpperCase()
+      )
+    })
+    it('should be render nothing if props is empty', () => {
+      const testRenderDetailWithMultipleValuePokemon = renderer.create(
+        <RenderDetailWithMultipleValuePokemon />
+      )
+      expect(testRenderDetailWithMultipleValuePokemon.toJSON()).toMatchSnapshot()
+    })
+  })
+  describe('Render Detail Pokemon', () => {
+    const testRenderDetailPokemon = renderer.create(
+      <RenderDetailPokemon selectedPokemon={mockSelectedPokemon} />
+    )
+    it('should be match to snapsot', () => {
+      expect(testRenderDetailPokemon.toJSON()).toMatchSnapshot()
+    })
+    it('name pokemon must be same with props', () => {
+      const instance = testRenderDetailPokemon.root
+      expect(
+        instance.findAllByType(BodyRegular)[0].props.children)
+          .toBe(mockSelectedPokemon.name.toUpperCase()
+      )
+    })
+    it('should be render nothing if props is empty', () => {
+      const testRenderDetailPokemon = renderer.create(
+        <RenderDetailPokemon />
+      )
+      expect(testRenderDetailPokemon.toJSON()).toMatchSnapshot()
+    })
+  })
+  describe('Render Pokemon', () => {
+    const testRenderPokemon = renderer.create(
+      <RenderPokemon selectedPokemon={mockPokemons[2]} />
+    )
+    it('should be match to snapsot', () => {
+      expect(testRenderPokemon.toJSON()).toMatchSnapshot()
+    })
+    it('should be render frame selected if props equals to pokemon.id', () => {
+      const testRenderPokemon = renderer.create(
+        <RenderPokemon selectedPokemon={mockPokemons[2]} selectedPokemonId={mockSelectedPokemonId} />
+      )
+      expect(testRenderPokemon.toJSON()).toMatchSnapshot()
+    })
+    it('should be render nothing if props is empty', () => {
+      const testRenderPokemon = renderer.create(
+        <RenderPokemon />
+      )
+      expect(testRenderPokemon.toJSON()).toMatchSnapshot()
+    })
+  })
+  describe('Render list Pokemons', () => {
+    const testRenderListPokemon = renderer.create(
+      <RenderListPokemon
+        pokemons={mockPokemons}
+        RenderItemPokemon={mockRenderItemPokemon}
+        handleRefresh={mockHandleRefresh}
+        handleLoadMore={mockHandleLoadMore}
+        refreshing={false}
+      />
+    )
+    it('should be match to snapsot', () => {
+      expect(testRenderListPokemon.toJSON()).toMatchSnapshot()
+    })
+    it('should be activity indicator if resfeshing true', () => {
+      const testRenderListPokemon = renderer.create(
+        <RenderListPokemon pokemons={mockPokemons} refreshing={true} />
+      )
+      expect(testRenderListPokemon.toJSON()).toMatchSnapshot()
+    })
+    it('should be render nothing if props is empty', () => {
+      const testRenderListPokemon = renderer.create(
+        <RenderListPokemon />
+      )
+      expect(testRenderListPokemon.toJSON()).toMatchSnapshot()
+    })
+  })
+  describe('Render Container Detail Pokemons', () => {
+    it('should be match to snapsot', () => {
+      const testRenderContainerDetailPokemon = renderer.create(
+        <RenderContainerDetailPokemon selectedPokemon={mockSelectedPokemon} loaderPokemonDetail={false} />
+      )
+      expect(testRenderContainerDetailPokemon.toJSON()).toMatchSnapshot()
+    })
+    it('should be activity indicator if loaderPokemonDetail true', () => {
+      const testRenderContainerDetailPokemon = renderer.create(
+        <RenderContainerDetailPokemon selectedPokemon={mockSelectedPokemon} loaderPokemonDetail={true} />
+      )
+      expect(testRenderContainerDetailPokemon.toJSON()).toMatchSnapshot()
+    })
+    it('should be render placeholder if props is empty', () => {
+      const testRenderContainerDetailPokemon = renderer.create(
+        <RenderContainerDetailPokemon />
+      )
+      expect(testRenderContainerDetailPokemon.toJSON()).toMatchSnapshot()
+    })
+  })
+})
--- screens/ComponentPokemon.js
+++ screens/ComponentPokemon.js
@@ -4,97 +4,115 @@ import { Item, Section, RenderIf } from "../components/index"
 import BodyRegular from "../components/UI/BodyRegular"
 import Colors from '../constants/colors'
 
-const RenderDetailWithOneValuePokemon = ({ text, value }) => (
-  <Item plain row style={styles.detailBox}>
-    <Item backgroundColor={Colors.lightGrey} center small width={'30%'}>
-      <BodyRegular>{text}</BodyRegular>
-    </Item>
-    <Item small row horizontal width={'70%'}>
-      <BodyRegular>{value}</BodyRegular>
-    </Item>
-  </Item>
-)
-
-export const RenderTypeName = ({ name, isFilter }) => (
-  <Item plain row style={styles.multipleContainerText}>
-    <BodyRegular style={{ color: Colors.primary }}>{name.toUpperCase()}</BodyRegular>
-    <RenderIf condition={isFilter}>
-      <Item plain style={styles.containerXButton}>
-        <BodyRegular style={styles.xButton}>X</BodyRegular>
-      </Item>
-    </RenderIf>
-  </Item>
-)
-
-const RenderDetailWithMultipleValuePokemon = ({ text, value }) => (
-  <Item plain row style={styles.detailBox}>
-    <Item backgroundColor={Colors.lightGrey} center small width={'30%'}>
-      <BodyRegular>{text}</BodyRegular>
-    </Item>
-    <Item small row horizontal width={'70%'}>
-      {
-        value.map((data) => {
-          return (
-            <RenderTypeName key={data.slot} name={data.type.name} />
-          )
-        })
-      }
-    </Item>
-  </Item>
-)
-
-export const RenderDetailPokemon = ({ selectedPokemon }) => {
-  return selectedPokemon && (
-    <Item plain>
-      <Item plain>
-        <Item width={'100%'} small center backgroundColor={Colors.lightGrey}>
-          <BodyRegular>{selectedPokemon.name.toUpperCase()}</BodyRegular>
+export const RenderDetailWithOneValuePokemon = ({ text, value }) => {
+  if (text && value) {
+    return (
+      <Item plain row style={styles.detailBox}>
+        <Item backgroundColor={Colors.lightGrey} center small width={'30%'}>
+          <BodyRegular>{text}</BodyRegular>
+        </Item>
+        <Item small row horizontal width={'70%'}>
+          <BodyRegular>{value}</BodyRegular>
         </Item>
       </Item>
-      <RenderDetailWithMultipleValuePokemon text={'Type'} value={selectedPokemon.types} />
-      <RenderDetailWithOneValuePokemon text={'Height'} value={`${selectedPokemon.height}"`} />
-      <RenderDetailWithOneValuePokemon text={'Weight'} value={`${selectedPokemon.weight} lbs.`} />
-    </Item>
-  )
+    )
+  } else return null
+}
+
+export const RenderTypeName = ({ name, isFilter }) => {
+  if ( name ) {
+    return (
+      <Item plain row style={styles.multipleContainerText}>
+        <BodyRegular style={{ color: Colors.primary }}>{name.toUpperCase()}</BodyRegular>
+        <RenderIf condition={isFilter}>
+          <Item plain style={styles.containerXButton}>
+            <BodyRegular style={styles.xButton}>X</BodyRegular>
+          </Item>
+        </RenderIf>
+      </Item>
+    )
+  } else return null
+}
+
+export const RenderDetailWithMultipleValuePokemon = ({ text, value }) => {
+  if (text && value && value.length > 1) {
+    return (
+      <Item plain row style={styles.detailBox}>
+        <Item backgroundColor={Colors.lightGrey} center small width={'30%'}>
+          <BodyRegular>{text}</BodyRegular>
+        </Item>
+        <Item small row horizontal width={'70%'}>
+          {
+            value.map((data) => {
+              return (
+                <RenderTypeName key={data.slot} name={data.type.name} />
+              )
+            })
+          }
+        </Item>
+      </Item>
+    )
+  } else return null
+}
+
+export const RenderDetailPokemon = ({ selectedPokemon }) => {
+  if (selectedPokemon) {
+    return (
+      <Item plain>
+        <Item plain>
+          <Item width={'100%'} small center backgroundColor={Colors.lightGrey}>
+            <BodyRegular>{selectedPokemon.name.toUpperCase()}</BodyRegular>
+          </Item>
+        </Item>
+        <RenderDetailWithMultipleValuePokemon text={'Type'} value={selectedPokemon.types} />
+        <RenderDetailWithOneValuePokemon text={'Height'} value={`${selectedPokemon.height}"`} />
+        <RenderDetailWithOneValuePokemon text={'Weight'} value={`${selectedPokemon.weight} lbs.`} />
+      </Item>
+    )
+  } else return null
 }
 
 export const RenderPokemon = ({ pokemon, handleSelectedPokemon, selectedPokemonId }) => {
-  return (
-    <TouchableOpacity onPress={() => handleSelectedPokemon(pokemon.id)}>
-      <Item
-        center plain
-        key={pokemon.id}
-        style={pokemon.id === selectedPokemonId ? styles.pokemonContainerSelected : styles.pokemonContainer}
-      >
-        <Image source={{uri: pokemon.imageUri}} style={ styles.pokemonImage} />
-      </Item>
-    </TouchableOpacity>
-  )
+  if (pokemon) {
+    return (
+      <TouchableOpacity onPress={() => handleSelectedPokemon(pokemon.id)}>
+        <Item
+          center plain
+          key={pokemon.id}
+          style={pokemon.id === selectedPokemonId ? styles.pokemonContainerSelected : styles.pokemonContainer}
+        >
+          <Image source={{uri: pokemon.imageUri}} style={ styles.pokemonImage} />
+        </Item>
+      </TouchableOpacity>
+    )
+  } else return null
 }
 
 export const RenderListPokemon = ({ refreshing, pokemons, RenderItemPokemon, handleRefresh, handleLoadMore }) => {
-  return (
-    <Item small center borderRadius={12} backgroundColor={Colors.whiteGrey02} height={'63%'}>
-      <RenderIf condition={!refreshing}>
-        <FlatList
-          data={pokemons}
-          keyExtractor={(item) => item.id}
-          renderItem={RenderItemPokemon}
-          horizontal={false}
-          numColumns={5}
-          refreshing={refreshing}
-          onRefresh={handleRefresh}
-          onEndReached={handleLoadMore}
-          onEndReachedThreshold={0.3}
-        />
-      </RenderIf>
-      <RenderIf condition={refreshing}>
-        <Item style={styles.containerActivtyIndicator}>
-          <ActivityIndicator size="large" color={Colors.blue} />
-        </Item>
-      </RenderIf>
-    </Item>
-  )
+  if (pokemons) {
+    return (
+      <Item small center borderRadius={12} backgroundColor={Colors.whiteGrey02} height={'63%'}>
+        <RenderIf condition={!refreshing}>
+          <FlatList
+            data={pokemons}
+            keyExtractor={(item) => item.id}
+            renderItem={RenderItemPokemon}
+            horizontal={false}
+            numColumns={5}
+            refreshing={refreshing}
+            onRefresh={handleRefresh}
+            onEndReached={handleLoadMore}
+            onEndReachedThreshold={0.3}
+          />
+        </RenderIf>
+        <RenderIf condition={refreshing}>
+          <Item style={styles.containerActivtyIndicator}>
+            <ActivityIndicator size="large" color={Colors.blue} />
+          </Item>
+        </RenderIf>
+      </Item>
+    )
+  } return null
 }
 
 export const RenderContainerDetailPokemon = ({ selectedPokemon, loaderPokemonDetail }) => {
@@ -138,7 +156,7 @@ const styles = StyleSheet.create({
     margin: 8
   },
   pokeballImage: {
-    height: Dimensions.get('window').height /5,
+    height: Dimensions.get('window').height / 5,
     resizeMode: 'contain',
     alignSelf: 'center'
   },
